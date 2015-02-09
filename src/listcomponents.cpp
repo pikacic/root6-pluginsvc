539bc0e0b14bb100f016c624a314462472561e71
Benedikt Hegner <hegner@cern.ch>
2015-02-09 14:48:35 +0100

    simplify using C++11

--- src/listcomponents.cpp
+++ src/listcomponents.cpp
@@ -10,6 +10,7 @@
 \*****************************************************************************/
 
 /// @author Marco Clemencic <marco.clemencic@cern.ch>
+/// @author Benedikt Hegner <benedikt.hegner@cern.ch>
 
 #include <iostream>
 #include <string>
@@ -21,44 +22,24 @@
 #include <Gaudi/PluginService.h>
 
 int main(int argc, char* argv[]) {
-  Gaudi::PluginService::Details::Registry &reg =
-      Gaudi::PluginService::Details::Registry::instance();
-  typedef Gaudi::PluginService::Details::Registry::KeyType key_type;
+  auto& reg = Gaudi::PluginService::Details::Registry::instance();
 
   // cache to keep track of the loaded factories
-  std::map<key_type, std::string> loaded;
-  {
-    // initialize the local cache
-    std::set<key_type> base = reg.loadedFactories();
-    for (std::set<key_type>::const_iterator f = base.begin(); f != base.end(); ++f)
-    {
-      loaded.insert(std::make_pair(*f, std::string("<preloaded>")));
-    }
-  }
+  auto loaded = reg.loadedFactories();
 
   // loop over the list of libraries passed on the command line
   for (int i = 1; i < argc; ++i) {
-
     if (dlopen(argv[i], RTLD_LAZY | RTLD_LOCAL)) {
-
-      std::set<key_type> factories = reg.loadedFactories();
-      std::set<key_type>::const_iterator f;
-      for (f = factories.begin(); f != factories.end(); ++f) {
-        if (loaded.find(*f) == loaded.end())
-        {
-          std::cout << argv[i] << ":" << *f << std::endl;
-          loaded[*f] = argv[i];
+      for(auto f : reg.loadedFactories() ){
+        if (loaded.find(f) == loaded.end()) {
+	  std::cout << argv[i] << ":" << f << std::endl;
+        } else {
+          std::cerr << "WARNING: factory '" << f << "already found previously" << std::endl; 
         }
-        else
-          std::cerr << "WARNING: factory '" << *f
-                    << "' already found in " << loaded[*f]
-                    << std::endl;
-      }
-
+      }     
     } else {
       std::cerr << "ERROR: failed to load " << argv[i] << std::endl;
     }
   }
-
   return 0;
 }
